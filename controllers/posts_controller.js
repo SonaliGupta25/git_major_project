51f746c00bebdff33cf3297d5a586302c6db47b6
SG <guptasonali2503@gmail.com>
2019-08-16 12:37:55 +0530

    add more router and controllers

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.name = function(req, res){
+   return res.end('<h1>Hey Enter Your Name !!</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,11 @@ const homeController = require('../controllers/home_controller');
 console.log('Router loaded');
 
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
+
+router.use('/posts',require('./posts'));
+
+//for any further routers..define it here 
+// router.use('/routerName',require('./routerFile'));
 
 module.exports = router;
