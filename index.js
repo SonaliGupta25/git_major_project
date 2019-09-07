2cb2f821fd4d81a6e0cce65b00ad013aa9fee511
SG <guptasonali2503@gmail.com>
2019-09-07 13:22:02 +0530

    Adding Partials

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 8002;
 
 // use express router
 app.use('/', require('./routes'));
+app.use(express.static('assests'));
 
+app.set('view engine','ejs');
+//app.set('views','./views');
 
+app.set('views',path.join(__dirname,'views'));
+
+var todoList = [
+    {
+        description: "Why not add a task",
+        date : "MAY 2,2019",
+        category: ""
+    },
+    {
+        description: "Let's Make a TODO App",
+        date : "APR 28,2019",
+        category: "SCHOOL"
+    },
+    {
+        description: "Annual report submission deadline",
+        date : "JUN 1,2019",
+        category: "WORK"
+    }
+]
+
+app.get('/',function(req, res){
+      
+    return res.render('home',{
+        title: "TODO App",
+        //todo_list: todoList
+    });
+});
 
 app.listen(port, function(err){
      
