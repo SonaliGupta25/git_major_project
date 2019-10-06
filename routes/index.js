1a4a7786200562f5ad3ae24ec5b03cca650d9276
SonaliGupta25 <guptasonali2503@gmail.com>
2019-10-06 20:20:59 +0530

    Adding Comments for each Post

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ console.log('Router loaded');
 router.get('/',homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
+router.use('/comments', require('./comments'));
 
 //for any further routers..define it here 
 // router.use('/routerName',require('./routerFile'));
