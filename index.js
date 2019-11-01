e79d1d7826072b7f1714475dc938db2dce913b92
Sonali Gupta <49031160+SonaliGupta25@users.noreply.github.com>
2019-11-01 22:53:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const sassMiddleware = require('node-sass-middleware');
 const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(sassMiddleware({
     src:'./assests/scss',
@@ -57,6 +59,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+app.use(flash());
+
+app.use(customMware.setFlash);
 
 // use express router
 app.use('/', require('./routes'));
---
+++ users_controller.js
@@ -0,0 +1,104 @@
+const User = require('../models/user');
+
+
+module.exports.profile = function(req, res){
+   // return res.end('<h1>Users Profile</h1>');
+   User.findById(req.params.id, function(err, user){
+    return res.render('user_profile',{
+        title: 'Users',
+        profile_user: user
+    });
+   });
+   
+}
+
+module.exports.update = function(req, res){
+
+   if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }
+        else{
+            return res.status(401).send('Unauthorized');        
+    }
+}
+
+module.exports.signUp = function(req, res){
+
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_up', {
+        title: "Codeial|Sign Up"
+    });
+
+};
+module.exports.signIn = function(req, res){
+
+
+
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
+        title: "Codeial|Sign In"
+    });
+
+};
+
+module.exports.create = function(req, res){
+    //TODO later
+
+    console.log('____________________----body')
+     console.log(req.body) ;
+
+
+     console.log(req.body.password,'---------------------') ;
+     console.log(req.body.confirm_password) ;
+
+
+
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+   
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){
+            console.log('error in finding user in signing up');
+             return;
+            }
+
+        if (!user){
+            User.create(req.body, function(err, user){
+                if(err){
+                    console.log('error in creating user while signing up');
+                     return;
+                    }
+
+
+                 
+                 console.log(user) ;   
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    });
+
+}
+module.exports.createSession = function(req, res){
+    //TODO later
+    req.flash('success','Signed In Successfully');
+    return res.redirect('/');
+
+}
+
+module.exports.destroySession = function(req, res){
+    req.logout();
+    req.flash('success','You have logged out');
+        return res.redirect('/');
+}
